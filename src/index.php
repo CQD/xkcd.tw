0fa929c30e6b49722e465ad2188dfb1a5ec41def
CQD <thecqd@gmail.com>
2017-07-31 05:17:56 +0800

    首頁加上 JSON-LD

--- src/index.php
+++ src/index.php
@@ -7,8 +7,23 @@ header('Cache-Control: public, max-age=3600'); // cache 1 小時
 $og['title'] = 'xkcd 中文翻譯';
 $og['url'] = 'http://xkcd.tw';
 
+$ld = [
+    "@context" => "http://schema.org/",
+    "@type" => "ComicSeries",
+    "description" => "某個關於浪漫、諷刺、數學、以及語言的漫畫",
+    "name" => "xkcd",
+    "sameAs" => "https://xkcd.com",
+    "inLanguage" => "zh-Hant-TW",
+    "author" => [
+        "@type" => "Person",
+        "name" => "Randall Munroe",
+        "sameAs" => 'https://en.wikipedia.org/wiki/Randall_Munroe',
+    ],
+];
+
 echo $twig->render('index.twig', [
     'page_title' => '首頁',
     'strips' => $strips,
     'og' => $og,
+    'ld' => $ld,
 ]);
