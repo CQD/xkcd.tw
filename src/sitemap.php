b0ac3387cf77b0b798ae570af1e3d4dc0dd79190
CQD <cqd@hiigara.net>
2018-12-04 02:34:51 +0800

    Sitemap 加上 314 年齡計算機與 936 密碼產生器

--- src/sitemap.php
+++ src/sitemap.php
@@ -6,4 +6,6 @@ echo '<'?>?xml version="1.0" encoding="UTF-8"?>
 <?php foreach ($strips as $id => $strip): ?>
 <url><loc>https://xkcd.tw/<?=(int) $id?></loc></url>
 <?php endforeach ?>
+<url><loc>https://xkcd.tw/s/314_date_calculator.html</loc></url>
+<url><loc>https://xkcd.tw/s/936_password_generator.html</loc></url>
 </urlset>
