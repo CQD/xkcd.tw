0842c575814b5c52b211cb89d7a89acde42ed5be
CQD <cqd@hiigara.net>
2020-11-02 23:30:31 +0800

    Sitemap 加上圖片資訊標注

--- src/sitemap.php
+++ src/sitemap.php
@@ -1,10 +1,18 @@
 <?php
 header('Content-Type: application/xml');
 echo '<'?>?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 <url><loc>https://xkcd.tw/</loc> </url>
 <?php foreach ($strips as $id => $strip): ?>
-<url><loc>https://xkcd.tw/<?=(int) $id?></loc></url>
+<url>
+<loc>https://xkcd.tw/<?=(int) $id?></loc>
+<image:image>
+<image:loc>https://xkcd.tw<?=$strip['img_url']?></image:loc>
+<image:title><?=htmlspecialchars($strip['title'])?></image:title>
+<image:license>https://creativecommons.org/licenses/by-nc/2.5/</image:license>
+</image:image>
+</url>
 <?php endforeach ?>
 <url><loc>https://xkcd.tw/s/314_date_calculator.html</loc></url>
 <url><loc>https://xkcd.tw/s/936_password_generator.html</loc></url>
